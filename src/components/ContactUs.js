0444703ea95d865e31bd02a10baa560c6441aa69
VISHNU P <90571792+vishnuponneth@users.noreply.github.com>
2024-10-20 21:40:53 +0530

    contact us updated

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -3,12 +3,18 @@ import React from 'react';
 const ContactUs = () => {
   return (
     <div style={styles.container}>
-      <h1>Vishnu ponneth CEO</h1>
+      <h1>Tech Fusion</h1>
+      <h2>Fill the form and we will get Back to you soon</h2>
+      <button id="myButton">form</button><br/>
+      <h3>Contact Us :</h3>
+      <h4>Email:Techfusioneducation.info@gmail.com<br/>Whatsapp:+91 7012422309</h4>
       {/* Add your contact information here */}
     </div>
   );
 };
-
+document.getElementById("myButton").onclick = function () {
+  window.location.href = "https://forms.gle/B4ApbWvVZ7BndnMB7"; // Link to the URL
+};
 const styles = {
   container: {
     padding: '20px',
